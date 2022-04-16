030f4f3b590b48359e75989f6ba8189912646966
Jin <3344513542@qq.com>
2022-04-16 19:22:07 +0800

    预制布局

--- src/views/merchants.vue
+++ src/views/merchants.vue
@@ -1,29 +1,95 @@
 <template>
-  <!-- App.vue -->
+  <v-app id="inspire">
+    <v-system-bar app>
+      <v-spacer></v-spacer>
 
-  <v-app>
-    <v-navigation-drawer app>
+      <v-icon>mdi-square</v-icon>
 
+      <v-icon>mdi-circle</v-icon>
+
+      <v-icon>mdi-triangle</v-icon>
+    </v-system-bar>
+
+    <v-navigation-drawer
+        v-model="drawer"
+        app
+    >
+      <v-sheet
+          color="grey lighten-4"
+          class="pa-4"
+      >
+        <v-avatar
+            class="mb-4"
+            color="grey darken-1"
+            size="64"
+        ></v-avatar>
+
+        <div>john@vuetifyjs.com</div>
+      </v-sheet>
+
+      <v-divider></v-divider>
+
+      <v-list>
+        <v-list-item
+            v-for="[icon, text] in links"
+            :key="icon"
+            link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
     </v-navigation-drawer>
 
-    <v-app-bar app>
-      <!-- --><v-btn elevation="2">管理员登录</v-btn>
-    </v-app-bar>
-
-    <!-- 根据应用组件来调整你的内容 -->
     <v-main>
+      <v-container
+          class="py-8 px-6"
+          fluid
+      >
+        <v-row>
+          <v-col
+              v-for="card in cards"
+              :key="card"
+              cols="12"
+          >
+            <v-card>
+              <v-subheader>{{ card }}</v-subheader>
 
-      <!-- 给应用提供合适的间距 -->
-      <v-container fluid>
+              <v-list two-line>
+                <template v-for="n in 6">
+                  <v-list-item
 
-        <!-- 如果使用 vue-router -->
-        <router-view></router-view>
+                      :key="n"
+                  >
+                    <v-list-item-avatar color="grey darken-1">
+                    </v-list-item-avatar>
+
+                    <v-list-item-content>
+                      <v-list-item-title>Message {{ n }}</v-list-item-title>
+
+                      <v-list-item-subtitle>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil repellendus distinctio similique
+                      </v-list-item-subtitle>
+                    </v-list-item-content>
+                  </v-list-item>
+
+                  <v-divider
+                      v-if="n !== 6"
+                      :key="`divider-${n}`"
+                      inset
+                  ></v-divider>
+                </template>
+              </v-list>
+            </v-card>
+          </v-col>
+        </v-row>
       </v-container>
     </v-main>
-
-    <v-footer app>
-      <!-- -->
-    </v-footer>
   </v-app>
 </template>
 
@@ -36,8 +102,32 @@ export default {
   name: 'merchants',
   components: {
   },
+  data: () => ({
+    cards: ['Today', 'Yesterday'],
+    drawer: null,
+    links: [
+      ['mdi-inbox-arrow-down', 'Inbox'],
+      ['mdi-send', 'Send'],
+      ['mdi-delete', 'Trash'],
+      ['mdi-alert-octagon', 'Spam'],
+    ],
+  }),
   beforeCreate() {
-
+    let that = this;
+    const token = this.$store.state.token;
+    userRequest({method: 'get', url: '/api/Login/QRCode/'+this.uuid+'/Valid'},  { headers: {  Authorization: token  } })
+        .then((response) => {
+          console.log(response.statusCode);
+          if(response.statusCode === 200){
+            that.$store.commit('setToken', response.data.token)
+            router.replace('/merchants')
+          }else{
+            console.log(response.message);
+          }
+        })
+        .catch(err => {
+          console.error(err);
+        })
   }
 }
 </script>
