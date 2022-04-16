4f68bdff03658d3082865953cc833b6d7e39529e
Jin <3344513542@qq.com>
2022-04-16 10:22:03 +0800

    解决跨域问题，引入QR库

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import store from './store'
 import vuetify from './plugins/vuetify'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@fortawesome/fontawesome-free/css/all.css'
+import {Axios} from "axios";
 
 Vue.config.productionTip = false
 
@@ -12,5 +13,8 @@ new Vue({
   router,
   store,
   vuetify,
+  Axios,
   render: h => h(App)
 }).$mount('#app')
+
+
---
+++ src/network/request.js
@@ -0,0 +1,44 @@
+import axios from 'axios'
+export let baseUrl = window.location.hostname //'118.190.97.125:8080'
+export let wsPort = 8080
+export let basePort = window.location.port
+let baseURL = '/api'
+
+export function request(config) {
+    axios.defaults.withCredentials = true;
+
+    const query = axios.create({
+        baseURL
+    });
+
+    query.interceptors.response.use(res => {
+        return res.data;
+    },error => {
+
+    })
+
+    return query(config);
+}
+
+export function userRequest(config) {
+    axios.defaults.withCredentials=true;
+
+    const query = axios.create({
+        baseURL
+    });
+
+    query.interceptors.response.use(res => {
+        if (res.data.code === 401 || res.data.code === 20004) {
+
+        }
+        return res.data;
+    },error => {
+    })
+
+    query.interceptors.request.use(config => {
+
+        return config
+    })
+
+    return query(config);
+}
