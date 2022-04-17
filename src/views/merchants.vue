af74e310632fc40ab628164c8a4a7b044f649e8b
Jin <3344513542@qq.com>
2022-04-17 21:18:13 +0800

    创建商家页

--- src/views/merchants.vue
+++ src/views/merchants.vue
@@ -2,8 +2,57 @@
 
   <div class="container">
     <div v-if="$store.state.isShopkeeper === true && $store.state.hasShop === true">
-      23
+
+      <v-navigation-drawer v-model="drawer" app permanent >
+        <v-list>         <!--店铺信息-->
+          <v-list-item link>
+            <v-list-item-content>
+              <v-list-item-title class="text-h6">
+                {{ shopInfo.name }}
+              </v-list-item-title>
+              <v-list-item-subtitle>{{ shopInfo.openTime }} - {{ shopInfo.closeTime }}</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-list nav dense>  <!--account-->
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon large color="teal darken-2">
+                mdi-email
+              </v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>My Files</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>mdi-account-multiple</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Shared with me</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>mdi-star</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Starred</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
+
+      <v-main>
+        <v-container class="py-8 px-6" fluid>
+
+        </v-container>
+      </v-main>
     </div>
+
+
+
     <div v-else="$store.state.isShopkeeper === true && $store.state.hasShop === false" class="container">
       <v-btn x-large color="success" dark v-on:click="openShop">
         开设店铺
@@ -31,23 +80,40 @@ export default {
     this.checkHasShop()
     this.checkManager()
   },
-  data: {},
+  data() {
+    return{
+      drawer:'',
+      shopInfo:''    //店铺的注册信息
+    }
+  },
   methods:{
-    ValidShopkeeper : function(){
+    getOrderByShopId:function (){
       userRequest({
         method: 'get',
-        url: '/api/Login/Valid/Shopkeeper',
+        url: '/api/Order/Shop/'+ this.$data.shopInfo.id,
       }).then((response) => {
         if(response.statusCode === 200){
-          store.state.isShopkeeper = true
+
         }else{
           console.log(response.message);
-          // store.state.isShopkeeper = false
         }
       }).catch(err => {
-          console.error(err);
-          router.replace('/')
-        })
+        console.error(err);
+      })
+    },
+    getShopInfoById:function (){
+      userRequest({
+        method: 'get',
+        url: '/api/Shop/'+ this.$data.shopInfo.id,
+      }).then((response) => {
+        if(response.statusCode === 200){
+
+        }else{
+          console.log(response.message);
+        }
+      }).catch(err => {
+        console.error(err);
+      })
     },
     checkHasShop:function (){
       userRequest({
@@ -56,6 +122,11 @@ export default {
       }).then((response) => {
         if(response.statusCode === 404 && response.data.shop === null){
           store.state.hasShop = false
+        }else if(response.statusCode === 200){
+          store.state.hasShop = true
+          this.$data.shopInfo = response.data.shop
+          this.getShopInfoById()
+          this.getOrderByShopId()
         }else{
           console.log(response.message);
         }
@@ -76,6 +147,22 @@ export default {
         }
       }).catch(err => {
         console.error(err);
+      })
+    },
+    ValidShopkeeper : function(){
+      userRequest({
+        method: 'get',
+        url: '/api/Login/Valid/Shopkeeper',
+      }).then((response) => {
+        if(response.statusCode === 200){
+          store.state.isShopkeeper = true
+        }else{
+          console.log(response.message);
+          // store.state.isShopkeeper = false
+        }
+      }).catch(err => {
+        console.error(err);
+        router.replace('/')
       })
     },
     openShop:function (){
