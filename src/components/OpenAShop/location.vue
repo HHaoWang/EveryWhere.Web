4d700df7ac73b8df8970cf3739e60b5c1c95b888
Jin <3344513542@qq.com>
2022-04-17 16:27:55 +0800

    实现级联选择器

--- src/components/OpenAShop/location.vue
+++ src/components/OpenAShop/location.vue
@@ -2,19 +2,72 @@
 
 
 <div>
-
+  <v-select
+      v-model="myProvince"
+      :items="this.$data.area"
+      item-text="name"
+      label="Standard"
+      return-object
+  ></v-select>
+  <v-select
+      v-model="myCity"
+      :items="this.$data.myProvince.subAreas"
+      item-text="name"
+      label="Standard"
+      return-object
+  ></v-select>
+  <v-select
+      v-model="myRegion "
+      :items="this.$data.myCity.subAreas"
+      item-text="name"
+      label="Standard"
+      return-object
+  ></v-select>
 </div>
 
 </template>
 
 <script>
 import router from "@/router";
+import {userRequest} from "@/network/request";
+import store from "@/store";
 
 export default {
   name: "location",
-  methods: {
-
+  created() {
+    this.getArea()
   },
+  data () {
+    return {
+      area:'',
+      myProvince:'',
+      myCity:'',
+      myRegion:''
+    }
+  },
+  methods: {
+    getArea(){
+      const that = this;
+      userRequest({
+        method: 'get',
+        url: '/api/Area/Tree',
+      }).then((response) => {
+        if(response.statusCode === 200){
+          that.$data.area = response.areasTree
+          console.log(that.$data.area);
+        }else{
+          console.log(response.message);
+        }
+      }).catch(err => {
+        console.error(err);
+      })
+    }
+  },
+  watch:{
+    myCity(){
+      console.log(this.$data.mySubAreas);
+    }
+  }
 }
 </script>
 
